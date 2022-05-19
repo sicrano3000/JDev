cc621a4089df8ae973fa58ea7ef928c5ecd38582
Jonathan Crespo <sicrano3000@gmail.com>
2022-05-19 18:35:40 -0300

    Criando um Consumer

---
+++ compra/src/main/java/com/mscompra/service/rabbitmq/RabbitmqConfig.java
@@ -0,0 +1,19 @@
+package com.mscompra.service.rabbitmq;
+
+import org.springframework.amqp.core.Queue;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitmqConfig {
+
+    @Value("${queue.name}")
+    private String queueName;
+
+    @Bean
+    public Queue queue() {
+        return new Queue(queueName, true);
+    }
+
+}
